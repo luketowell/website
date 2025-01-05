9e1b1a7498b0c2901b299b822c45fd18e64d64cb
Luke Towell <luke.towell@hotmail.com>
2025-01-05 18:34:58 +0000

    blog: Updated the content to make post summary more dynamic

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("posts", views.all_posts, name="posts"),
+    path("posts", views.posts, name="posts"),
     path("posts/<slug>", views.single_post, name="post-detail-page")
 ]
