0f91359afcbeb1fe7331d381a82501c9e20290a4
Luke Towell <luke.towell@hotmail.com>
2025-01-14 22:45:14 +0000

    Added Post Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils.text import slugify
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+# Post Model
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    date = models.DateField(auto_now=True)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True db_index = True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super.save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.slug} - {self.title} (Published on: {self.date})"
