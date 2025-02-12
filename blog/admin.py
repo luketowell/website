b8196ae2a55e3d5c3547069bd19bcaa53068170b
Luke Towell <luke.towell@hotmail.com>
2025-02-12 22:17:39 +0000

    comments: updated the admin console to have Comments Models displayed

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Author, Tag
+from .models import Post, Author, Tag, Comment
 
 # Register your models here.
 
@@ -12,7 +12,11 @@ class AuthorAdmin(admin.ModelAdmin):
     list_filter = ("first_name", "last_name")
     list_display = ("id", "first_name", "last_name")
 
+class CommentAdmin(admin.ModelAdmin):
+    list_filter = ("user_name", "user_email", "post")
+    list_display = ("id", "user_name", "user_email", "text", "post")
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(Tag)
+admin.site.register(Comment, CommentAdmin)
