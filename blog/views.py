d304109dbd6ce1fb43be906790bea9e47bed53ba
Luke Towell <luke.towell@hotmail.com>
2025-02-13 22:53:32 +0000

    read-later: configured and styled the read-later button also added functionality to add and remove posts to read later session

--- blog/views.py
+++ blog/views.py
@@ -28,13 +28,24 @@ class AllPostView(ListView):
 
 class SinglePostView(View):
 
+    def isStoredPost(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+            is_saved_for_later = post_id in stored_posts
+        else:
+            is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        
         context = {
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": CommentForm(),
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.isStoredPost(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -54,12 +65,45 @@ class SinglePostView(View):
             "post": post,
             "post_tags": post.tags.all(),
             "comment_form": comment_form,
-            "comments": post.comments.all().order_by("-id")
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.isStoredPost(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
 
 
 class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+        context={
+                
+            }
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+            context["has_posts"] = False
+        else: 
+            posts = Post.objects.filter(id__in = stored_posts)
+
+            context["posts"] = posts
+            context["has_posts"]=True
+            print(f"posts: {context["posts"]}")
+            
+        
+        return render(request, "blog/stored-posts.html", context)
 
     def post(self, request):
-        pass
+        post_id = request.POST["post_id"]
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+            
+        if int(post_id) not in stored_posts:
+            stored_posts.append(int(request.POST["post_id"]))
+            request.session["stored_posts"] = stored_posts
+        else: 
+            print(f"stored post pre-removal: {stored_posts}")
+            stored_posts.remove(int(post_id))
+            request.session["stored_posts"] = stored_posts
+
+        print(f"stored post post-removal: {stored_posts}")
+        return HttpResponseRedirect("/")
